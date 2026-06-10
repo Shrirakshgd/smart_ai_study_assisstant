47dea2a50445179e512c4f898c2770cb11319c6d
Shrirakshgd <shrirakshgd18@gmail.com>
2026-06-10 13:02:04 +0530

    resolved auth

--- backend/test_load.py
+++
@@ -1,9 +0,0 @@
-import os
-from services import load_note_context
-
-try:
-    load_note_context("This is a test note content.")
-    print("Success")
-except Exception as e:
-    import traceback
-    traceback.print_exc()
